3a2bef453e5843912ca3606784d8bb3600d1fa59
Amritesh <amriteshc101@icloud.com>
2022-06-06 12:25:23 +0530

    Stuff was added

--- client/src/pages/VolunteerFormPage/index.jsx
+++ client/src/pages/VolunteerFormPage/index.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+
+// css
+import styles from "./VolunteerFormPage.module.css";
+
+//assets
+import Wave from "../../assets/Top_wave.svg";
 
 const VolunteerFormPage = () => {
-  return (
-    <div>VolunteerFormPage</div>
-  )
+    return (
+        <main className={styles.container}>
+            <div className={styles.mainContainer}>
+                <div className={styles.leftContainer}>
+                    hi
+                    {/*https://stackoverflow.com/questions/41078478/css-animated-checkmark*/}
+                </div>
+                <div className={styles.rightContainer}>
+                    double hi
+                </div>
+            </div>
+            <img 
+                src={Wave}
+                className={styles.wave}
+            />
+        </main>
+    );
 }
 
 export default VolunteerFormPage
