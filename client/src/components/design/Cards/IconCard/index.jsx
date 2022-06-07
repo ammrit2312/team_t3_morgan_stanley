ef1648385034018eacd0b5cda7a0f2c0112a57f1
Amritesh <amriteshc101@icloud.com>
2022-06-08 02:04:09 +0530

    Added activity card

--- client/src/components/design/Cards/IconCard/index.jsx
+++ client/src/components/design/Cards/IconCard/index.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 
-const IconCard = ({title, Icon, detail}) => {
-    return (
-        <div>
-            {Icon}
-            {title}
-            {detail}
-        </div>
-    );
-}
- 
+// css
+import styles from "../Cards.module.css";
+
+/**
+ * @param {String} title Title of the card
+ * @param {React.Component} Icon Icon to be displayed in the card
+ * @param {String} detail Detail of the card
+ * @param {Object} customStyles Styles for the card div if required
+ * @author ammrit2312 <amriteshc101@icloud.com>
+ * @returns {React.Component} Card for Icons
+ */
+
+const IconCard = ({ title, Icon, detail, customStyles = {} }) => {
+  return (
+    <div className={styles.iconContain} style={customStyles}>
+      {Icon}
+      <div className={styles.iconTitle}>{title}</div>
+      <p className={styles.iconDetails}>{detail}</p>
+    </div>
+  );
+};
+
 export default IconCard;
--- client/src/pages/Dashboards/Volunteer/index.jsx
+++ client/src/pages/Dashboards/Volunteer/index.jsx
@@ -6,13 +6,46 @@ import styles from "../Dashboards.module.css";
 // components
 import VolunteerDashboardCard from "../../../components/design/Cards/VolunteerDashboardCard";
 
+// test
+const testData = [
+    {
+        title: "Story Telling",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut expedita vero, odio quam eum dolore. Quidem temporibus et, magnam voluptates suscipit ullam dicta delectus tempora quas voluptatum in earum corrupti natus similique possimus odio placeat doloribus excepturi odit, libero aliquam eos ducimus itaque accusamus. Tenetur?",
+        date: "23/06/2022",
+        time: "11 am",
+        mode: "online",
+        duration: "1 hour",
+    },
+    {
+        title: "Meeting the kids",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut expedita vero, odio quam eum dolore. Quidem temporibus et, magnam voluptates suscipit ullam dicta delectus tempora quas voluptatum in earum",
+        date: "02/06/2022",
+        time: "1 pm",
+        mode: "offline",
+        duration: "2 hours",
+        location: "Gokuldham Complex, Powder Gali, Goregaon East, Mumbai,Maharashtra - 400063",
+    },
+    {
+        title: "Office Work",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut expedita vero, odio quam eum dolore. Quidem temporibus et, magnam voluptates suscipit ullam dicta delectus tempora quas voluptatum in earum",
+        date: "15/06/2022",
+        time: "12 pm",
+        mode: "offline",
+        duration: "4 hours",
+        location: "Gokuldham Complex, Powder Gali, Goregaon East, Mumbai,Maharashtra - 400063",
+    },
+]
+
 const VolunteerDashboard = () => {
+
+    // send onAccept and onReject to VolunteerDashboardCard
+    
     return (
         <div>
             <h1>Volunteer Dashboard</h1>
-            <VolunteerDashboardCard />
-            <VolunteerDashboardCard />
-            <VolunteerDashboardCard />
+            {testData.map((data, index) => (
+                <VolunteerDashboardCard key={index} {...data} />
+            ))}
         </div>
     );
 }
