27fbdb296632e2080dd19d27ea307756e4e1c68f
Amritesh <amriteshc101@icloud.com>
2022-06-11 20:53:55 +0530

    Volunteer profile done

--- client/src/pages/ShowProfileVolunteers/index.jsx
+++ client/src/pages/ShowProfileVolunteers/index.jsx
@@ -12,7 +12,7 @@ import IconCard from "../../components/design/Cards/IconCard";
 
 // icons
 import { FaUserAlt, FaBuilding, FaWifi } from "react-icons/fa";
-import { MdEmail, MdOutlineWork, MdHearing } from "react-icons/md";
+import { MdEmail, MdOutlineWork, MdHearing, MdAssignmentInd } from "react-icons/md";
 import { AiFillPhone } from "react-icons/ai";
 import { IoSchool, IoLanguage } from "react-icons/io5";
 import { ImLocation2 } from "react-icons/im";
@@ -20,6 +20,7 @@ import { ImLocation2 } from "react-icons/im";
 import { FcGlobe, FcLike } from "react-icons/fc";
 import { BsCalendarDateFill } from "react-icons/bs";
 import { GiSkills } from "react-icons/gi";
+import { CgUnavailable } from "react-icons/cg";
 
 const ShowProfileVolunteer = () => {
     let { id } = useParams();
@@ -73,31 +74,39 @@ const ShowProfileVolunteer = () => {
                 <h1 className={`${styles.title} ${styles.cards} ${styles.colorDetails}`}>{data["Volunteer_Name"]}</h1>
                 <div className={styles.cardsContainer}>
                     {/* Basic Details */}
-                    <div className={`${styles.cards} ${styles.containerCard}`}>
-                        <IconCard
-                            title="Name"
-                            icon={<FaUserAlt size={20} color={colors.PRIMARY_GREEN}/>}
-                            detail={data["Volunteer_Name"]}
-                            customStyles={{
-                                width: "5rem",
-                            }}
-                        />
-                        <IconCard
-                            title="Email"
-                            icon={<MdEmail size={25} color={colors.PRIMARY_BLUE}/>}
-                            detail={data["Volunteer_Email"]}
-                        />
-                        <IconCard
-                            title="Number"
-                            icon={<AiFillPhone size={25} color={colors.PRIMARY_GREEN}/>}
-                            detail={data["Volunteer_Number"]}
-                        />
-                        <IconCard
-                            title="College"
-                            icon={<IoSchool size={25}/>}
-                            detail={data["Volunteer_College"]}
-                        />
-                        <div style={{gridColumn: "2 / 4"}}>
+                    <div>
+                        <h1 className={`${styles.title} ${styles.cards} ${styles.colorDetails}`}>Basic Details</h1>
+                        <div className={`${styles.cards} ${styles.containerCard}`}>
+                            <IconCard
+                                title="Name"
+                                icon={<FaUserAlt size={20} color={colors.PRIMARY_GREEN}/>}
+                                detail={data["Volunteer_Name"]}
+                                // customStyles={{
+                                //     width: "5rem",
+                                // }}
+                            />
+                            <IconCard
+                                title="Email"
+                                icon={<MdEmail size={25} color={colors.PRIMARY_BLUE}/>}
+                                detail={data["Volunteer_Email"]}
+                            />
+                            <IconCard
+                                title="Number"
+                                icon={<AiFillPhone size={25} color={colors.PRIMARY_GREEN}/>}
+                                detail={data["Volunteer_Number"]}
+                            />
+                            <IconCard
+                                title="College"
+                                icon={<IoSchool size={25}/>}
+                                detail={data["Volunteer_College"]}
+                            />
+                            {/* <div style={{gridColumn: "2 / 4"}}>
+                                <IconCard
+                                    title="Address"
+                                    icon={<ImLocation2 size={25} color={colors.PRIMARY_GREEN}/>}
+                                    detail={data["Volunteer_Address"]}
+                                />
+                            </div> */}
                             <IconCard
                                 title="Address"
                                 icon={<ImLocation2 size={25} color={colors.PRIMARY_GREEN}/>}
@@ -106,86 +115,119 @@ const ShowProfileVolunteer = () => {
                         </div>
                     </div>
 
-                    <div className={`${styles.cards} ${styles.containerCard}`}>
-                        <IconCard
-                            title="Nationality"
-                            icon={<FcGlobe size={25}/>}
-                            detail={data["Volunteer_Nationality"]}
-                            customStyles={{
-                                width: "5rem",
-                            }}
-                        />
-                        <IconCard
-                            title="Academic Qualifications"
-                            icon={<IoSchool size={25}/>}
-                            detail={data["Volunteer_Academic_Qualifications"]}
-                            customStyles={{
-                                width: "6rem",
-                            }}
-                        />
-                        <IconCard
-                            title="Occupation"
-                            icon={<MdOutlineWork size={25}/>}
-                            detail={data["Volunteer_Occupation"]}
-                            customStyles={{
-                                width: "6rem",
-                            }}
-                        />
-                        <IconCard
-                            title="Languages"
-                            icon={<IoLanguage size={25}/>}
-                            detail={data["Volunteer_Languages"].join(", ")}
-                            customStyles={{
-                                width: "5rem",
-                            }}
-                        />
-                        <div style={{gridColumn: "2 / 4"}}>
+                    <div>
+                        <h1 className={`${styles.title} ${styles.cards} ${styles.colorDetails}`}>Other Details</h1>
+                        <div className={`${styles.cards} ${styles.containerCard}`}>
+                            <IconCard
+                                title="Nationality"
+                                icon={<FcGlobe size={25}/>}
+                                detail={data["Volunteer_Nationality"]}
+                                // customStyles={{
+                                //     width: "5rem",
+                                // }}
+                            />
+                            <IconCard
+                                title="Occupation"
+                                icon={<MdOutlineWork size={25}/>}
+                                detail={data["Volunteer_Occupation"]}
+                                // customStyles={{
+                                    //     width: "6rem",
+                                // }}
+                            />
+                            <IconCard
+                                title="Languages"
+                                icon={<IoLanguage size={25}/>}
+                                detail={data["Volunteer_Languages"].join(", ")}
+                                // customStyles={{
+                                    //     width: "5rem",
+                                // }}
+                            />
+                            {/* <div style={{gridColumn: "2 / 4"}}>
+                                <IconCard
+                                title="Organization"
+                                icon={<FaBuilding size={22}/>}
+                                detail={data["Volunteer_Organization"]}
+                                />
+                            </div> */}
                             <IconCard
                                 title="Organization"
                                 icon={<FaBuilding size={22}/>}
                                 detail={data["Volunteer_Organization"]}
+                            />
+                            <IconCard
+                                title="Academic Qualifications"
+                                icon={<IoSchool size={25}/>}
+                                detail={data["Volunteer_Academic_Qualifications"]}
+                                // customStyles={{
+                                //     width: "6rem",
+                                // }}
                             />
                         </div>
                     </div>
                     
-                    <div className={`${styles.cards} ${styles.containerCard}`}>
-                        <IconCard
-                            title="Hear about us"
-                            icon={<MdHearing size={25}/>}
-                            detail={data["Volunteer_Platform"].join(", ")}
-                        />
-                        <IconCard
-                            title="Location Preferred"
-                            icon={<ImLocation2 size={25} color={colors.PRIMARY_GREEN}/>}
-                            detail={data["Volunteer_Preferred_Locations"].join(", ")}
-                        />
-                        <IconCard
-                            title="Availability"
-                            icon={<BsCalendarDateFill size={25}/>}
-                            detail={data["Volunteer_Availability"].join(", ")}
-                        />
-                        <IconCard
-                            title="Mode"
-                            icon={<FaWifi size={25}/>}
-                            detail={data["Volunteer_Preferred_Mode"]}
-                        />
-                        <IconCard
-                            title="Skills"
-                            icon={<GiSkills size={25}/>}
-                            detail={data["Volunteer_Skills"].join(", ")}
-                        />
-                        <IconCard
-                            title="Preferences"
-                            icon={<FcLike size={25}/>}
-                            detail={data["Volunteer_Interested_Activity_Type"].join(", ")}
-                        />
-                        {/* <div style={{gridColumn: "2 / 4"}}>
+                    <div>
+                        <h1 className={`${styles.title} ${styles.cards} ${styles.colorDetails}`}>Preferences</h1>
+                        <div className={`${styles.cards} ${styles.containerCard}`}>
                             <IconCard
-                                title="Organization"
-                                icon={<FaBuilding size={22}/>}
-                                detail={data["Volunteer_Organization"]}
+                                title="Hear about us"
+                                icon={<MdHearing size={25}/>}
+                                detail={data["Volunteer_Platform"].join(", ")}
                             />
-                        </div> */}
+                            <IconCard
+                                title="Location Preferred"
+                                icon={<ImLocation2 size={25} color={colors.PRIMARY_GREEN}/>}
+                                detail={data["Volunteer_Preferred_Locations"].join(", ")}
+                            />
+                            <IconCard
+                                title="Availability"
+                                icon={<BsCalendarDateFill size={25}/>}
+                                detail={data["Volunteer_Availability"].join(", ")}
+                            />
+                            <IconCard
+                                title="Mode"
+                                icon={<FaWifi size={25}/>}
+                                detail={data["Volunteer_Preferred_Mode"]}
+                            />
+                            <IconCard
+                                title="Skills"
+                                icon={<GiSkills size={25} color={colors.PRIMARY_ORANGE}/>}
+                                detail={data["Volunteer_Skills"].join(", ")}
+                            />
+                            <IconCard
+                                title="Preferences"
+                                icon={<FcLike size={25}/>}
+                                detail={data["Volunteer_Interested_Activity_Type"].join(", ")}
+                            />
+                            {/* <div style={{gridColumn: "2 / 4"}}>
+                                <IconCard
+                                    title="Organization"
+                                    icon={<FaBuilding size={22}/>}
+                                    detail={data["Volunteer_Organization"]}
+                                />
+                            </div> */}
+                        </div>
+                    </div>
+                    
+                    <div>
+                        <h1 className={`${styles.title} ${styles.cards} ${styles.colorDetails}`}>Statistics</h1>
+                        <div className={`${styles.cards} ${styles.containerCard}`}>
+                            <IconCard
+                                title="Assigned"
+                                icon={<MdAssignmentInd size={25}/>}
+                                detail={data["assigned"]? "Yes" : "No"}
+                            />
+                            <IconCard
+                                title="Opted out"
+                                icon={<CgUnavailable size={30} color={colors.PRIMARY_RED}/>}
+                                detail={data["Volunteer_Number_Of_Activities_Opted_Out"]}
+                            />
+                            <IconCard
+                                title="Attended Activities"
+                                icon={<BsCalendarDateFill size={25} color={colors.PRIMARY_GREEN}/>}
+                                detail={data["Volunteer_Number_Of_Activities_Attended"]}
+                            />
+                            {/* //show Upcoming_Activities */}
+                        </div>
                     </div>
                 </div>
             </div>
