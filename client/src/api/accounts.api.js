c526350bcd1c03e9b4c45cf2c8c59c03c3601e03
Mayank1403 <mayank1403@gmail.com>
2022-06-14 01:53:00 +0530

    Final final push

--- client/src/api/accounts.api.js
+++ client/src/api/accounts.api.js
@@ -1,18 +1,18 @@
 import { POST, GET, PUT } from "../config/api.config";
 
 export function createNewAccount(user) {
-  console.log(user);
-  const { UserID, Email, Filled_Form, AccountType} = user;
+  console.log("API USER",user);
+  // const { UserID, Email, Filled_Form, AccountType} = user;
 
   // Only the necessary data is being sent to the server
-  const userData = {
-    UserID,
-    Email,
-    Filled_Form,
-    AccountType,
-  };
+  // const userData = {
+  //   UserID,
+  //   Email,
+  //   Filled_Form,
+  //   AccountType,
+  // };
 
-  return POST(`/api/user/post-user-details`, userData);
+  return POST(`/api/user/post-user-details`, user);
 }
 
 export function getExistingAccount(uid) {
